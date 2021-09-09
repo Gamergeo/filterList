ed0a3f33238d18543e154e017870c453b5621a2c
gleva <gleva@192.168.0.25>
2021-09-09 15:43:31 +0200

    Ajout de collapsable

--- filterList/src/main/resources/js/element/filterList.js
+++ filterList/src/main/resources/js/element/filterList.js
@@ -1,12 +1,26 @@
 /**
  * Initialise le filter list
  */
-$.fn.filterList = function () {
+$.fn.filterList = function (collapsable) {
 		
 	return this.each(function() {
+		
 		let filterList = $(this);
-		let filter = filterList.children().eq(0);
-		let list = filterList.children().eq(1);
+		let filter;
+		let list;
+		if (collapsable) {
+			let title = filterList.children().eq(0);
+			filter = filterList.children().eq(1);
+			list = filterList.children().eq(2);
+			
+			title.addClass('plugin_filterList_filterTitle')
+			
+			title.collapsable(filter);
+			
+		} else {
+			filter = filterList.children().eq(0);
+			list = filterList.children().eq(1);
+		}
 		
 		filterList.addClass('plugin_filterList_filterList');
 		filter.addClass('plugin_filterList_filter');
